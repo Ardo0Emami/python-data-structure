08865e12abda6c6d7342580f0e38916a1d9540f6
Ardo0Emami <88878432+Ardo0Emami@users.noreply.github.com>
2025-08-25 20:50:08 -0700

    Add add_front and add_back methods with unit tests for DoublyLinkedList

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -2,9 +2,11 @@ from typing import Generic, TypeVar, Optional
 
 T = TypeVar("T")
 
+
 class DoublyNode(Generic[T]):
-    def __init__(self, value: T, prev: Optional["DoublyNode[T]"] = None,
-                  next: Optional["DoublyNode[T]"] = None) -> None:
+    def __init__(self, value: T,
+                 prev: Optional["DoublyNode[T]"] = None,
+                 next: Optional["DoublyNode[T]"] = None) -> None:
         self.value = value
         self.prev = prev
         self.next = next
@@ -18,6 +20,26 @@ class DoublyLinkedList(Generic[T]):
         self.head: Optional[DoublyNode[T]] = None
         self.tail: Optional[DoublyNode[T]] = None
         self._size = 0
+
+    def add_front(self, value: T) -> None:
+        new_node = DoublyNode(value, None, self.head)
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            self.head.prev = new_node
+            self.head = new_node
+
+        self._size += 1
+
+    def add_back(self, value: T) -> None:
+        new_node = DoublyNode(value, self.tail, None)
+        if not self.tail:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self._size += 1
 
     def __len__(self) -> int:
         return self._size
